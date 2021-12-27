93e93630aa99782a3a7f84babc5a764363ccc121
suryaajis330 <suryaajis330@gmail.com>
2021-12-27 16:24:08 +0700

    try insert one and many in mongo db

--- controllers/main-controller.go
+++ controllers/main-controller.go
@@ -29,3 +29,41 @@ func InsertMany(client *mongo.Client, ctx context.Context, dataBase, col string,
 	result, err := collection.InsertMany(ctx, docs)
 	return result, err
 }
+
+// var document interface{}
+
+// 	document = bson.D{
+// 		{"rollNo", 175},
+// 		{"maths", 80},
+// 		{"science", 90},
+// 		{"computer", 95},
+// 	}
+
+// var documents []interface{}
+// documents = []interface{}{
+// 	bson.D{
+// 		{"rollNo", 153},
+// 		{"maths", 65},
+// 		{"science", 59},
+// 		{"computer", 55},
+// 	},
+// 	bson.D{
+// 		{"rollNo", 162},
+// 		{"maths", 86},
+// 		{"science", 80},
+// 		{"computer", 69},
+// 	},
+// }
+
+// result, err := controllers.InsertOne(client, ctx, "bookshelf", "marks", document)
+// if err != nil {
+// 	panic(err)
+// }
+
+// fmt.Println(result)
+
+// manyResult, err := controllers.InsertMany(client, ctx, "bookshelf", "marks", documents)
+// if err != nil {
+// 	panic(err)
+// }
+// fmt.Println(manyResult)
--- models/user.go
+++ models/user.go
@@ -1,11 +1,20 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id"`
-	Name     *string            `json:"name" validate:"required"`
-	Email    *string            `json:"email" validate:"required,email"`
-	Password string             `json:"password" validate:"required,min=6"`
-	Phone    string             `bson:"phone" json:"phone"`
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       *string            `json:"name" validate:"required"`
+	Email      *string            `json:"email" validate:"required,email"`
+	Password   *string            `json:"password" validate:"required,min=6"`
+	Phone      *string            `json:"phone" validate:"required"`
+	Token      *string            `json:"token"`
+	Role       *string            `json:"role" validate:"required,eq=Admin|eq=User"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	User_id    string             `json:"user_id"`
 }
